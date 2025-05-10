2a2eefa9a49a9d1c5e5221c5f648ca2b08461fc7
Krzysztof <krzyskuar@gmail.com>
2025-05-10 02:04:42 +0200

    fixed issue regarding loading tf

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ CORS(app)
 model_name = "facebook/blenderbot-400M-distill"
 model = AutoModelForSeq2SeqLM.from_pretrained(
     model_name,
-    from_tf=True,
     from_flax=False,
     cache_dir=None,
     force_download=False,
@@ -34,15 +33,12 @@ def handle_prompt():
     data = request.get_data(as_text=True)
     data = json.loads(data)
     input_text = data['prompt']
-
     history = "\n".join(conversation_history)
     inputs = tokenizer.encode_plus(history, input_text, return_tensors="pt")
     outputs = model.generate(**inputs, max_length= 60)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
-
     conversation_history.append(input_text)
     conversation_history.append(response)
-
     return response
 
 if __name__ == '__main__':
